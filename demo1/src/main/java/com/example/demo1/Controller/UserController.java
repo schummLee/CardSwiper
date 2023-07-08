811bced714a7cf603f71728a72dc65730e8fc794
lee23 <71886857+schummLee@users.noreply.github.com>
2023-07-08 20:49:22 +0200

    Add files via upload

---
+++ demo1/src/main/java/com/example/demo1/Controller/UserController.java
@@ -0,0 +1,24 @@
+package com.example.demo1.Controllers;
+
+@RestController
+@RequestMapping("/users")
+public class MyRestController {
+
+    private final UserRepository userRepository; private final CustomerRepository customerRepository;
+
+    public MyRestController(UserRepository userRepository, CustomerRepository customerRepository) {
+        this.userRepository = userRepository; this.customerRepository = customerRepository;
+    }
+
+    @GetMapping("/{userId}")
+    public User getUser(@PathVariable Long userId) {return this.userRepository.findById(userId).get();}
+
+    @GetMapping("/{userId}/customers")
+    public List<Customer> getUserCustomers(@PathVariable Long userId) {
+        return this.userRepository.findById(userId).map(this.customerRepository::findByUser).get();
+    }
+
+    @DeleteMapping("/{userId}")
+    public void deleteUser(@PathVariable Long userId) { this.userRepository.deleteById(userId); }
+
+}
---
+++ demo1/src/main/java/com/example/demo1/Repositories/UserRepositories.java
@@ -0,0 +1,5 @@
+package com.example.demo1.Repositories;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    // ...
+}
